61244f94149566effb36b590b0d7df4571907f9a
yaren0600 <begumozturk0600@gmail.com>
2025-12-18 02:29:57 +0300

    18.12.25

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 let todoItems = [];
 
+const todoStorage = localStorage.getItem("todoItems");
+
+const todoListEl = document.querySelector(".todo_list");
+
+if(todoStorage){
+    //Eğer todoStorage değerinde bir veri varsa 
+    todoItems = [...JSON.parse(todoStorage)]; // array içerisine ekle 
+};
+
 const todoModalEl = document.querySelector(".todo_modal");
 
 
@@ -18,26 +27,33 @@ const uniqueIdGenerator = () => {
     return Math.round (Math.random() * 100000 + 1 ) ; //Math.random metodu normalde 0 ve 1 arasında sayı üretiyordu. Bu işlemle biz 1 ve 100000 arasında sayı üretilmesini sağladık.
     //Math.round metodu ise sayıyı integera dönüştürür.
 
-}
+};
 
 const addTodoToHtml = (todoItem) => {
-    const todoListEl = document.querySelector(".todo_list");
-    let todoItemHtml = `<li class="todo_item ${ todoItem.isComplete ? 'todo_item-complete' : ''}"> 
-        <div class="todo_complete btn">
-            <img src="./img/checked.svg"/>
+    let todoItemHtml = `<li class="todo_item ${
+         todoItem.isComplete ? 'todo_item-complete' : ''}"> 
+        <div class="todo_complete btn" data-id = "${todoItem.id}"
+            onclick = "toggleTodoComplete(this)">
+            <img  src="./img/checked.svg" />
         </div>
         <div class="todo_info">
             <span class="todo_info-title">${todoItem.title}</span>
             <span class="todo_info-desc">${todoItem.desc}</span>
         </div>
-        <img class="todo_delete-icon btn" src="./img/delete.svg"/>
+        <img class="todo_delete-icon btn" src="./img/delete.svg" 
+        data-id="${todoItem.id}" onclick = "removeTodoItem(this)"/>
     </li>`;
 
     todoListEl.insertAdjacentHTML("beforeend", todoItemHtml);
 };
 
+const saveTodoItemsToLS = () => {
+    localStorage.setItem("todoItems", JSON.stringify(todoItems));
+}
+
 //Başlık ve açıklaması girilen ve kaydedilen işin arraye aktarılması ve ekranda gösterilmesi gerekiyor.
 const addTodoItems = () => {
+    if(todoListEl.innerHTML != "") todoListEl.innerHTML = "";
     const title = document.querySelector("input[name = 'title']").value;
     const desc = document.querySelector("textarea[name = 'desc']").value;
 
@@ -45,12 +61,56 @@ const addTodoItems = () => {
         id : uniqueIdGenerator(), 
         title, 
         desc, 
-        isComplete: false
+        isComplete: false, 
     };
-    addTodoToHtml(addTodoItems);
+    document.querySelector("#addTodoForm").reset();
+    addTodoToHtml(addedTodoItem);
     todoItems.push (addedTodoItem);
+    saveTodoItemsToLS();
 
     toggleModal(); //İş kaydedildikten sonra formu kapatıyoruz
 
 };
+
+const toggleTodoComplete = (selectedEl) => {
+    const toggleItemIndex = todoItems.findIndex(todo => todo.id == selectedEl.dataset.id);
+
+    if(toggleItemIndex != -1){
+        todoItems[toggleItemIndex].isComplete = !todoItems[toggleItemIndex].isComplete;
+        selectedEl.parentNode.classList.toggle("complete");
+        saveTodoItemsToLS();
+    }
+    
+};
+
+const removeTodoItem = (removedEl) => {
+    const removedItemIndex = todoItems.findIndex(todo => todo.id == removedEl.dataset.id);
+
+    if(removedItemIndex != -1){
+        todoItems.splice(removedItemIndex, 1); //id'si removedItemIndex değeri olan değerden itibaren 1 veriyi siliyoruz.
+        removedEl.parentNode.remove();
+        saveTodoItemsToLS(); //Sayfayı yenilediğimiz zaman yapılan değişikliklerin unutulmamasını sağlar.
+        if(todoItems.length == 0){
+            const notFoundItem = `<li class="none-todos"> Henüz Yapılacak Bir İş Eklenmedi...</li>`
+            todoListEl.insertAdjacentHTML("beforeend", notFoundItem);
+        }
+    }
+}
+
+const listTodoItems = () => {
+    if(todoItems.length <= 0 ){
+        const notFoundItem = `<li class="none-todos"> Henüz Yapılacak Bir İş Eklenmedi...</li>`
+        todoListEl.insertAdjacentHTML("beforeend", notFoundItem);
+    }
+    else{
+        todoItems.forEach(todo => { //todoItems değişkenlerinin tümünü dönerek todo değişkenine atıyoruz
+        addTodoToHtml(todo); //todo değişkenlerinin HTML kısmında gösteriyoruz
+    });
+    }
+
+
+}
+listTodoItems();
+
+
 
